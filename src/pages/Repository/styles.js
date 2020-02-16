2a2e58d8a69bfdfc2b692dbc8bd5cd010f42fdcc
Sergio Cuadros <sergio@sitecbr.com.br>
2020-02-16 14:04:47 -0300

    <feauture> Added detailed info for issues. <todo> handle wrong repo name.

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -41,3 +41,63 @@ export const Owner = styled.header`
     text-align: center;
   }
 `;
+
+export const IssueList = styled.ul`
+  margin-top: 30px;
+  list-style: none;
+  padding-top: 30px;
+  border-top: 1px solid #eee;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border-radius: 4px;
+    border: 1px solid #eee;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          color: #333;
+          text-decoration: none;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          color: #333;
+          height: 20px;
+          font-size: 12px;
+          font-weight: 600;
+          background: #eee;
+          padding: 3px 4px;
+          margin-left: 10px;
+          border-radius: 2px;
+        }
+      }
+
+      p {
+        color: #999;
+        font-size: 12px;
+        margin-top: 5px;
+      }
+    }
+  }
+`;
